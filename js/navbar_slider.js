23978bc83d3aaeac2924d24f26fdc4fdba5f1dc9
Aleksey Kutsyba <aleksey.kutsyba@gmail.com>
2017-03-02 17:34:30 +0200

    Add files via upload

--- js/navbar_slider.js
+++ js/navbar_slider.js
@@ -5,9 +5,16 @@
 // });
 
 
+// $(".nav li").hover(function () {
+// 	if(window.matchMedia('(min-width: 992px)').matches)
+// 	{
+// 		$(this).children("ul").stop().delay(0).animate({height: "toggle", opacity: "toggle"}, 100);
+// 	};
+// });
+
 $(".nav li").hover(function () {
 	if(window.matchMedia('(min-width: 992px)').matches)
 	{
-		$(this).children("ul").stop().delay(0).animate({height: "toggle", opacity: "toggle"}, 100);
+		$(this).children("ul").stop().delay(0).animate({height: "toggle", opacity: "toggle"}, 700,  'easeOutElastic', function () {});
 	};
 });
